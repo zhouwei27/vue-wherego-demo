9e333cfdf97d9e5bf08831423b0a2a65f1bf2823
周薇-20670 <wei.zhou05@hand-china.com>
2018-10-11 16:40:14 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
+<div>
   <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
   </div>
+  <div class="search-content" ref="search" v-show="keyword">
+    <ul>
+      <li class="search-item" v-for="item of list" :key="item.id">{{item.name}}</li>
+      <li class="search-item" v-show="hasNoData">没有找到匹配数据</li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -29,4 +78,20 @@ export default {
       border-radius: .06rem
       color: #666
       border: none
+  .search-content
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    z-index: 1
+    background: #eee
+    overflow: hidden
+    font-size: .28rem
+    .search-item
+      line-height: .62rem
+      color: #666
+      background: #fff
+      padding-left: .2rem
+      border-bottom: 1px solid #ccc
 </style>
